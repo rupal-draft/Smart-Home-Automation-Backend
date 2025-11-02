fccef9159f0fe77746043ef6f4f0633b64cf9b20
Rupal Paul <rupalpaulgspr@gmail.com>
2025-11-03 02:27:33 +0530

    Added configuration yamls

--- src/main/java/com/shas/smart_home_automation_system/SmartHomeAutomationSystemApplication.java
+++ src/main/java/com/shas/smart_home_automation_system/SmartHomeAutomationSystemApplication.java
@@ -2,8 +2,12 @@ package com.shas.smart_home_automation_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class SmartHomeAutomationSystemApplication {
 
 	public static void main(String[] args) {
