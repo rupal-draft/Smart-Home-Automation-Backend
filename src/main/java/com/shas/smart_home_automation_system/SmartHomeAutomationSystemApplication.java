ca05d80725f032a5d5f710cdef90a75b0b974240
Rupal Paul <rupalpaulgspr@gmail.com>
2025-11-03 01:59:03 +0530

    Initial Setup

---
+++ src/main/java/com/shas/smart_home_automation_system/SmartHomeAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shas.smart_home_automation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeAutomationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeAutomationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shas/smart_home_automation_system/SmartHomeAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shas.smart_home_automation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeAutomationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
